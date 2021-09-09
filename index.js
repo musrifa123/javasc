371a97b501db259d86ddd2d5e9222c4a40a53ed6
musrifa <musrifamuhammed@gmail.com>
2021-09-09 12:07:43 +0530

    second

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.log("helllooo");
 console.log(document.getElementsByTagName("p")[0].innerText)
+function bsa()
+{
+    console.log("clickme")
+
+document.getElementsByClassName("blah")[0].innerText="hfdbfkdgkgjgnfjdgnfjdhkfdjhurtyu"
+document.getElementsByTagName("p".style.color="red"
+}
